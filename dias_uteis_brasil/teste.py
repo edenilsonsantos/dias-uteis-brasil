f34e63c3a56b05f45f4b99cdf0075003e20ed9cf
Edenilson F. Santos <47802746+edenilsonsantos@users.noreply.github.com>
2023-03-08 21:43:26 -0300

    Add files via upload

---
+++ dias_uteis_brasil/teste.py
@@ -0,0 +1,12 @@
+# criar arquivo classe
+# criar o setup.py
+# pip3 install setuptools wheel
+
+
+from dias_uteis_brasil import Dia_util
+
+Dia_util = Dia_util()
+dias_uteis_mes_atual = Dia_util.dias_uteis_mes()
+print()
+
+
